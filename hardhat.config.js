ffe423d09a3e1db033dc4b4f2412a62d57fb26be
Haritha2498 <haritha2015a@gmail.com>
2024-09-07 13:25:28 +0530

    m

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     infura:{
       url:"https://sepolia.infura.io/v3/66d60f103eac4256995259d73ede2b51",
-      accounts:["d2f2533358a02b95e5372feeda3b6015631625919f8e0067f1ecf298ad310f14"]
+      accounts:["your private key"]
     },
   },
   solidity: "0.8.20",
